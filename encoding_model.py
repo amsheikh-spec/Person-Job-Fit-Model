2a494fae3a17f97fe6dc7cb6d75f630f00a5b6bd
Ibrahim-gif <56799071+Ibrahim-gif@users.noreply.github.com>
2023-07-30 16:48:49 -0400

    Added BERT based classification and BERT based sliding window encoder

--- encoding_model.py
+++ encoding_model.py
@@ -2,76 +2,142 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
 
-def top_resumes(label, cosine_scores, resumes, designations):
-  values_array = np.array(cosine_scores).reshape(len(cosine_scores))
+def model_similarity(model, resumes, jd, designations, k):
+  # 'all-MiniLM-L6-v2'
+  print(f"Loading pre-trained {model} sentence encoding model")
+  model = SentenceTransformer(model)
+  print("Embedding resume dataset")
+  resume_embeddings = get_embeddings(resumes, model)
+  print("Embedding Job Description Dataset")
+  jd_embeddings = get_embeddings(jd, model)
+
+  jd = jd_embeddings[0]
+
+  print("Performing cosine similarity")
+  #compute cosine similarity of one jd with all the resumes
+  cosine_scores = similarity_score(resume_embeddings, jd)
+
+  print("Getting the top K resumes")
+  matching_resumes = top_k_resumes("web developer", cosine_scores, resumes, designations, k)
+  df = pd.DataFrame(matching_resumes, columns=['Resume', 'Score'])
+  print(df)
+  return df
+
+def get_embeddings(resumes, model):
+  resume_chunk_1 = []
+  resume_chunk_2 = []
+  resume_chunk_3 = []
+  resume_chunk_4 = []
+  resume_chunk_5 = []
+
+  #Lets consider the first 1000 tokens with an overlap of 50 tokens we perform split
+  for i in range(len(resumes)):
+    chunk_1 = []
+    chunk_2 = []
+    chunk_3 = []
+    chunk_4 = []
+    chunk_5 = []
+    chunks = resumes[i].split()
+    chunks_length = len(chunks)
+
+    if(chunks_length < 250):
+      chunk_1 = chunks
+    elif(chunks_length > 250 and chunks_length < 450):
+      chunk_1 = chunks[:250]
+      chunk_2 = chunks[200:]
+    elif(chunks_length > 450 and chunks_length < 650):
+      chunk_1 = chunks[:250]
+      chunk_2 = chunks[200:450]
+      chunk_3 = chunks[400:]
+    elif(chunks_length > 650 and chunks_length < 850):
+      chunk_1 = chunks[:250]
+      chunk_2 = chunks[200:450]
+      chunk_3 = chunks[400:650]
+      chunk_4 = chunks[600:]
+
+    elif(chunks_length > 850 and chunks_length < 1050):
+      chunk_1 = chunks[:250]
+      chunk_2 = chunks[200:450]
+      chunk_3 = chunks[400:650]
+      chunk_4 = chunks[600:850]
+      chunk_5 = chunks[800:]
+
+    elif(chunks_length > 1050):
+      chunk_1 = chunks[:250]
+      chunk_2 = chunks[200:450]
+      chunk_3 = chunks[400:650]
+      chunk_4 = chunks[600:850]
+      chunk_5 = chunks[800:1050]
+
+
+    resume_chunk_1.append(' '.join(chunk_1))
+    resume_chunk_2.append(' '.join(chunk_2))
+    resume_chunk_3.append(' '.join(chunk_3))
+    resume_chunk_4.append(' '.join(chunk_4))
+    resume_chunk_5.append(' '.join(chunk_5))
+
+
+  embed_1 = model.encode(resume_chunk_1)
+  embed_2 = model.encode(resume_chunk_2)
+  embed_3 = model.encode(resume_chunk_3)
+  embed_4 = model.encode(resume_chunk_4)
+  embed_5 = model.encode(resume_chunk_5)
+
+  embeddings = [[0]] * len(embed_1)
+  #combine Embeddings
+  for i in range(len(embed_1)):
+    embeddings[i] = []
+    embeddings[i].extend(embed_1[i])
+    if(i < len(embed_2)):
+      embeddings[i].extend(embed_2[i])
+    if(i < len(embed_3)):
+      embeddings[i].extend(embed_3[i])
+    if(i < len(embed_4)):
+      embeddings[i].extend(embed_4[i])
+    if(i < len(embed_5)):
+      embeddings[i].extend(embed_5[i])
+
+  return embeddings
+
+def similarity_score(resumes_embeddings, jd_embedding):
+  return util.cos_sim(resumes_embeddings, jd_embedding)
+
+def top_k_resumes(label, cosine_scores, resumes, designations, k):
+  values_array = np.array(cosine_scores).reshape(len(resumes))
 
   # Number of top values to find
-  k = 500
+  n = 500
 
   # Get the indices and values of the top k values
-  top_k_indices_values = sorted(enumerate(values_array), key=lambda x: x[1], reverse=True)[:k]
-  top_5 = []
+  top_k_indices_values = sorted(enumerate(values_array), key=lambda x: x[1], reverse=True)[:n]
+  top_k = []
   # Print the indices and values
   for index, value in top_k_indices_values:
-      if len(top_5) == 5:
+      if len(top_k) == k:
         break
-      if designations[index] == label and (resumes[index], cosine_scores[index]) not in top_5:
-        top_5.append((resumes[index], str(cosine_scores[index] * 100)))
+      if designations[index] == label and (resumes[index], cosine_scores[index]) not in top_k:
+        top_k.append((resumes[index], str(cosine_scores[index] * 100)))
       # print(f"resume: {designations[index]}, Value: {str((value * 100))}")
 
-  return top_5
+  return top_k
 
 
 def main():
-    data_resumes = pd.read_csv('processed data/preprocessed_resume.csv')
-    resumes = data_resumes["resume"]  # List of resumes
-    designations = data_resumes["label"]  # List of corresponding designations
+  data_resumes = pd.read_csv('/content/drive/MyDrive/MSCI_641/preprocessed_resume.csv')
+  resumes = data_resumes["resume"]  # List of resumes
+  designations = data_resumes["label"]  # List of corresponding designations
+  resumes = list(resumes)
+  data_jd = pd.read_csv('preprocessed_jd.csv')
+  jd = data_jd["job_description"]
+  jd = list(jd)
 
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    resume_embeddings = model.encode(resumes)
+  sum = 0
+  for i in range(len(resumes)):
+    tokens = resumes[i].split()
+    sum += len(tokens)
+  #average token length
+  print("Average resume token length is: ", sum/len(resumes))
 
-    data_jd = pd.read_csv('preprocessed_jd.csv')
-    jd = data_jd["job_description"]  # List of resumes
-    software_dev_jd = jd[0]
-    project_manager_jd = jd[4]
-    web_dev_jd = jd[12]
-    java_dev_jd = jd[15]
-    database_admin_jd = jd[34]
-    network_admin_jd = jd[2]
-    system_admin_jd = jd[21]
-
-    software_dev_jd = model.encode(software_dev_jd)
-    project_manager_jd = model.encode(project_manager_jd)
-    web_dev_jd = model.encode(web_dev_jd)
-    java_dev_jd = model.encode(java_dev_jd)
-    database_admin_jd = model.encode(database_admin_jd)
-    network_admin_jd = model.encode(network_admin_jd)
-    system_admin_jd = model.encode(system_admin_jd)
-
-    #Compute cosine-similarities for each sentence with each other sentence
-    soft_cosine_scores = util.cos_sim(resume_embeddings, software_dev_jd)
-    pm_cosine_scores = util.cos_sim(resume_embeddings, project_manager_jd)
-    wd_cosine_scores = util.cos_sim(resume_embeddings, web_dev_jd)
-    jvd_cosine_scores = util.cos_sim(resume_embeddings, java_dev_jd)
-    da_cosine_scores = util.cos_sim(resume_embeddings, database_admin_jd)
-    na_cosine_scores = util.cos_sim(resume_embeddings, network_admin_jd)
-    sa_cosine_scores = util.cos_sim(resume_embeddings, system_admin_jd)
-
-    #similarly we can find the top list of resumes for other job postings 
-    sd_resumes = top_resumes("software developer", soft_cosine_scores, resumes, designations)
-    pm_resumes = top_resumes("project manager", pm_cosine_scores, resumes, designations)
-    na_resumes = top_resumes("network administrator", na_cosine_scores, resumes, designations)
-    jvd_resumes = top_resumes("java developer", jvd_cosine_scores, resumes, designations)
-    wd_resumes = top_resumes("web developer", wd_cosine_scores, resumes, designations)
-    da_resumes = top_resumes("database administrator", da_cosine_scores, resumes, designations)
-    sa_resumes = top_resumes("systems administrator", sa_cosine_scores, resumes, designations)
-
-    df = pd.DataFrame(wd_resumes, columns=['Resume', 'Score'])
-    print(df)
-
-    label_map = {'software developer': 1, 'project manager': 2, 'web developer': 3, 'java developer': 4, 'database administrator': 5, 'network administrator': 6, 'systems administrator': 7}
-    data_resumes['job_title_label'] = data_resumes['label'].map(label_map)
-    data_jd['job_title_label'] = data_jd['title'].map(label_map)
-
-    data_resumes.head()
-
+  allMiniLML6v2 = model_similarity('all-MiniLM-L6-v2', resumes, jd, designations, 5)
+  allMiniLML12v2 = model_similarity('all-MiniLM-L12-v2', resumes, jd, designations, 5)
+  paraphrasealbertsmallv2 = model_similarity('paraphrase-albert-small-v2', resumes, jd, designations, 5)
---
+++ resume_classification.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, Input, Flatten, BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import SparseCategoricalAccuracy
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from transformers import AutoTokenizer, TFDistilBertForSequenceClassification
+from tensorflow.keras.utils import plot_model
+from wordcloud import WordCloud
+
+df = pd.read_csv("data/preprocessed_resume.csv")
+df.head(5)
+
+labels_dict = {}
+
+for idx, label in enumerate(df.label.unique()):
+    labels_dict[label] = idx
+
+labels_dict
+
+df.label = df.label.apply(func=lambda x: labels_dict[x])
+df.label = df.label.astype(np.int64)
+
+def text_preprocess(data,col):
+    data[col] = data[col].apply(func=clean_html)
+    data[col] = data[col].apply(func=remove_)
+    data[col] = data[col].apply(func=removeStopWords)
+    data[col] = data[col].apply(func=remove_digits)
+    data[col] = data[col].apply(func=remove_links)
+    data[col] = data[col].apply(func=remove_special_characters)
+    data[col] = data[col].apply(func=punct)
+    data[col] = data[col].apply(func=non_ascii)
+    data[col] = data[col].apply(func=email_address)
+    data[col] = data[col].apply(func=lower)
+    return data
+
+df.head()
+tokenizer = AutoTokenizer.from_pretrained("manishiitg/distilbert-resume-parts-classify")
+bert_model = TFDistilBertForSequenceClassification.from_pretrained("manishiitg/distilbert-resume-parts-classify",from_pt=True)
+
+train_df, test_df = train_test_split(df,test_size=0.3,shuffle=True,random_state=101)
+df.describe()
+#Text Tokenization
+X_train = tokenizer(text=train_df.resume.tolist(),
+                   add_special_tokens=True,
+                   padding=True,
+                   truncation=True,
+                   max_length=1050,
+                   return_tensors='tf',
+                   return_attention_mask=True,
+                   return_token_type_ids=False,
+                   verbose=1)
+
+X_test = tokenizer(text=test_df.resume.tolist(),
+                  add_special_tokens=True,
+                  padding=True,
+                  truncation=True,
+                  max_length=1050,
+                  return_tensors='tf',
+                  return_attention_mask=True,
+                  return_token_type_ids=False,
+                  verbose=1)
+max_resume_len = 1050
+
+input_ids = Input(shape=(max_resume_len,),dtype=tf.int32,name='input_ids')
+attention_masks = Input(shape=(max_resume_len,),dtype=tf.int32,name='attention_mask')
+
+word_embeddings = bert_model(input_ids,attention_mask=attention_masks)[0] # 0 --> final hidden state, 1 --> pooling output
+
+output = Flatten()(word_embeddings)
+output = Dense(units=1024,activation='relu')(output)
+output = BatchNormalization()(output)
+output = Dropout(0.25)(output)
+output = Dense(units=512,activation='relu')(output)
+output = Dropout(0.25)(output)
+output = Dense(units=256,activation='relu')(output)
+output = BatchNormalization()(output)
+output = Dropout(0.25)(output)
+output = Dense(units=128,activation='relu')(output)
+output = Dropout(0.25)(output)
+output = Dense(units=64,activation='relu')(output)
+output = Dense(units=7,activation='softmax')(output)
+
+model = Model(inputs=[input_ids,attention_masks],outputs=output)
+model.layers
+
+
+model.layers[2].trainable = True
+model.summary()
+plot_model(model,to_file='model.png',dpi=100,show_shapes=True)
+
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=5e-5,
+    decay_steps=10000,
+    decay_rate=0.01)
+adam = Adam(learning_rate=lr_schedule)
+
+# adam = Adam(learning_rate=,
+#            epsilon=2e-8,
+#            decay=0.01,
+#            clipnorm=1.0)
+
+model.compile(loss='sparse_categorical_crossentropy',optimizer=adam,metrics=SparseCategoricalAccuracy('balanced_accuracy'))
+
+es = EarlyStopping(monitor='val_balanced_accuracy',patience=250,verbose=1,mode='max',restore_best_weights=True)
+mc = ModelCheckpoint('resume_parser.h5',monitor='val_balanced_accuracy',mode='max',verbose=1,save_best_only=True)
+
+r = model.fit(x={'input_ids': X_train['input_ids'], 'attention_mask': X_train['attention_mask']},
+             y=train_df.label,
+             epochs=2,
+             batch_size=8,
+             callbacks=[es,mc],
+             validation_data=({'input_ids': X_test['input_ids'], 'attention_mask': X_test['attention_mask']},test_df.label))
+
+loss, acc = model.evaluate({'input_ids': X_test['input_ids'], 'attention_mask': X_test['attention_mask']},test_df.label)
+print("Test Sparse Categorical Crossentropy Loss:", loss)
+print("Test Balanced Categorical Accuracy:", acc)
+
+test_predictions = model.predict({'input_ids': X_test['input_ids'], 'attention_mask': X_test['attention_mask']})
+test_predictions = np.argmax(test_predictions,axis=1)
+test_predictions
+
+print("Confusion Matrix:")
+print(confusion_matrix(test_df.label,test_predictions))
+print("Classification Report:")
+print(classification_report(test_df.label,test_predictions))
+
+plt.plot(r.history['loss'],'r',label='train loss')
+plt.plot(r.history['val_loss'],'b',label='test loss')
+plt.xlabel('No. of Epochs')
+plt.ylabel('Categorical Crossentropy Loss')
+plt.title('Loss Graph')
+plt.legend()
+
+plt.plot(r.history['balanced_accuracy'],'r',label='train accuracy')
+plt.plot(r.history['val_balanced_accuracy'],'b',label='test accuracy')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Balanced Categorical Accuracy')
+plt.title('Accuracy Graph')
+plt.legend()
