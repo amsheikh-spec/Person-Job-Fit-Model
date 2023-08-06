ead04bc3d945f84bce4d4ae30d598778e1aca132
Ibrahim-gif <56799071+Ibrahim-gif@users.noreply.github.com>
2023-08-05 23:04:24 -0400

    Final changes

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import msci_641_jd_cleaning
 import resume_data_extraction
 import resume_preprocessing
 import encoding_model
+import resume_classification
 
 steps = [("Job Description Data Prepration", msci_641_jd_cleaning.main()),
          ("Resume Data Extraction", resume_data_extraction.main()),
          ("Resume Data Pre Preprocessing", resume_preprocessing.main()),
-         ("Generating Embeddings and Calculating scores", encoding_model.main())]
-# msci_641_jd_cleaning.main()
-# resume_data_extraction.main()
+         ("Generating Embeddings and Calculating scores", encoding_model.main()),
+         ("Model Classification of the Job title for the resume", resume_classification.main())]
 
 pipe = Pipeline(steps=steps)
 
